3dd12214cdc04d9d3c0631b12ccbf02cfe69471b
Leonardo Sardella <todosobremiclub@gmail.com>
2026-08-02 20:11:30 -0300

    Restaurar redireccion de buildDir en android/build.gradle.kts (necesaria para que flutter build apk encuentre el .apk)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,6 +10,18 @@ buildscript {
     }
 }
 
+// 👇 Redirige la carpeta de build a la raíz del proyecto (build/), que es donde Flutter espera encontrar el .apk final
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
 allprojects {
     repositories {
         google()
